1fafdd4aabafecba03639641c0fa241b1f7cd77a
mazensaid-02 <mazenbensaid548@gmail.com>
2025-10-04 23:52:22 +0100

    Adding app.use(express.json());

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,9 @@ const User = require ('../models/User')
 
 exports.createUser = async (req, res)=>{
     try{
-        const user = new User(req,res);
-        const saved = await user.saved();
+        //const { name, email, password } = req.body;
+        const user = new User(req.body);
+        const saved = await user.save();
         res.status(201).json(saved);
     } catch (err) {
         res.status(400).json({error: err.message});
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
-
+const userRoutes = require('./routes/userRoutes');
 const app = express();
+const mongoose = require('mongoose');
 
+
+mongoose.connect('mongodb://localhost:27017/users').then(()=>{
+    console.log('db connected');
+    
+}).catch(err=>{
+    console.log('db not connected',err.message);
+    
+})
+app.use(express.json());
+app.use('/api/users', userRoutes);
 
 
 
